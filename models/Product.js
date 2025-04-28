401827ae4053979c311f4ac15a7cf4012b283521
NamanPundir <pundirnaman245@example.com>
2025-04-28 09:22:16 +0530

    save

--- models/Product.js
+++ models/Product.js
@@ -4,8 +4,8 @@ const productSchema = new mongoose.Schema({
     userId:{type: String, required:true},
     name:{type: String, required:true},
     description:{type: String, required:true},
-    price:{type: Number, required:true},
-    offerPrice:{type: Number, required:true},
+    price:{type: String, required:true},
+    offerPrice:{type: String, required:true},
     image:{type: Array, required:true},
     category:{type: String, required:true},
     date:{type: Number, required:true}
