331876ff7245dca5b8df17610eec123542674292
NamanPundir <pundirnaman245@example.com>
2025-04-28 10:22:10 +0530

    save

--- assets/assets.js
+++ assets/assets.js
@@ -138,8 +138,8 @@ export const productsDummyData = [
     "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
     "name": "Apple AirPods Pro 2nd gen",
     "description": "Apple AirPods Pro (2nd Gen) with MagSafe Case (USB-C) provide excellent sound, active noise cancellation, and a comfortable fit. The USB-C case ensures quick charging, and they pair seamlessly with Apple devices for an effortless audio experience.",
-    "price": "₹499.99",
-    "offerPrice": "₹399.99",
+    "price": 499.99,
+    "offerPrice": 399.99,
     "image": [
       "https://raw.githubusercontent.com/avinashdm/gs-images/main/quickcart/k4dafzhwhgcn5tnoylrw.webp",
       "https://raw.githubusercontent.com/avinashdm/gs-images/main/quickcart/j212frakb8hdrhvhajhg.webp",
